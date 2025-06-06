6728867744302b0c28b3f5e0e9533aaf7a5e6efe
Sebastian Hidalgo <sebashidmar@gmail.com>
2025-06-06 17:18:37 -0500

    Fix: fixed the conection to the database and finalice login

--- src/turing/chatbot/views.py
+++ src/turing/chatbot/views.py
@@ -1,11 +1,15 @@
+# chatbot/views.py
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-@login_required # Asegura que solo usuarios logueados puedan acceder
+@login_required
 def chatbot_view(request):
-    # Logica para el chatbot y sus vistas a implementar 
     context = {
-        'username': request.user.username,
-        # Pasaremos mas datos a futuro
+        # Opciones para mostrar información del usuario:
+        'user_email': request.user.email,                 # Email del usuario
+        'user_name': request.user.name,                   # Nombre
+        'user_last_name': request.user.last_name,         # Apellido
+        'user_full_name': request.user.get_full_name(),   # Método si lo definiste
+        # Elige el que quieras pasar a la plantilla
     }
     return render(request, 'chatbot/chat_interface.html', context)
