8a88baa627788c330857c25b5545999702229bf5
Santiago Arboleda Velasco <santiagoarboledavelasco@gmail.com>
2025-10-14 12:20:44 -0500

    fix: add page of courses

--- src/turing/teachers/urls.py
+++ src/turing/teachers/urls.py
@@ -30,4 +30,6 @@ urlpatterns = [
     path('tutoring-schedules/', TutoringScheduleListView.as_view(), name='tutoring_schedules'),
     path('courses/<int:course_pk>/upload-schedule/', TutoringScheduleUploadView.as_view(), name='upload_schedule'),
     path('groups/<int:group_pk>/manage-tutoring/', manage_tutoring_slots, name='manage_tutoring'),
+    path('courses/<int:pk>/', ManageCourseView.as_view(), name='manage_course'),
+
 ]
